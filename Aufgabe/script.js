041ca68a25db81d951ae20ec59f16a4f287d54e2
Sebastian Schoeneich <business.sschoeneich@gmail.com>
2024-06-24 13:50:20 +0200

    Added additional functionality (Task 21)

--- Aufgabe/script.js
+++ Aufgabe/script.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("btn_welcome_message").onclick = () => {
         try {
             date = new Date().toLocaleTimeString("en-GB", {hour12: false, hour: "numeric", minute: "numeric"});
-            alert("Wilkommen auf der Website\n\nWie geht es dir heute?\n\nAktuell ist es " + date + "\nDu nutzt folgenden Browser: " + navigator.userAgent);
+            alert("Wilkommen auf der Website\n\
+                  \nWie geht es dir heute?\n\
+                  \nAktuell ist es " + date + "\n\
+                  \nDu nutzt folgenden Browser: " + navigator.userAgent + "\n\
+                  \nDeine aktuelle Fenstergröße beträgt: " + window.innerWidth + "x" + window.innerHeight);
     
             let button = document.getElementById("btn_welcome_message");
             var parent = button.parentNode;
