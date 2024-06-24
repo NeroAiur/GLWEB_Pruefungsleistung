960f77b36ebd499aa07244bec69bdb407d7f147b
Sebastian Schöneich <Sebastian Schneich>
2024-06-24 16:49:17 +0200

    Added functionality to have the welcome message button reappear (Task 24)

--- Aufgabe/script.js
+++ Aufgabe/script.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let wm_btn_pressed = false;
     document.getElementById("btn_welcome_message").onclick = () => {
         try {
-            date = new Date().toLocaleTimeString("en-GB", {hour12: false, hour: "numeric", minute: "numeric"});
-            alert("Wilkommen auf der Website\n\
-                  \nWie geht es dir heute?\n\
-                  \nAktuell ist es " + date + "\n\
-                  \nDu nutzt folgenden Browser: " + navigator.userAgent + "\n\
-                  \nDeine aktuelle Fenstergröße beträgt: " + window.innerWidth + "x" + window.innerHeight);
-    
-            let button = document.getElementById("btn_welcome_message");
-            var parent = button.parentNode;
-            parent.removeChild(button);
-            button.remove();
-            
-            var ul = document.getElementById("lst_navigation");
-            var li = document.createElement("li");
-            var btn_reap = document.createElement("button");
-            btn_reap.id = "btn_reap";
-            li.appendChild(btn_reap);
-            ul.appendChild(li);
+            if (wm_btn_pressed === false) {
+                date = new Date().toLocaleTimeString("en-GB", {hour12: false, hour: "numeric", minute: "numeric"});
+                alert("Wilkommen auf der Website\n\
+                       \nWie geht es dir heute?\n\
+                       \nAktuell ist es " + date + "\n\
+                       \nDu nutzt folgenden Browser: " + navigator.userAgent + "\n\
+                       \nDeine aktuelle Fenstergröße beträgt: " + window.innerWidth + "x" + window.innerHeight);
+        
+                let button = document.getElementById("btn_welcome_message");
+                button.innerHTML = "Return Welcome Message";
+                wm_btn_pressed = true;
+
+            } else if (wm_btn_pressed === true) {
+                let button = document.getElementById("btn_welcome_message");
+                button.innerHTML = "Welcome Message";
+                wm_btn_pressed = false;
+            }
+
         } catch(error) {
-            console.log("lele");
+            console.log("Error Welcome-Message-Button");
         }
     }
 })
